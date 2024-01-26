e7d1651f7a2f1ba625cdaae4a60bc96359041209
isomer04 <43922158+isomer04@users.noreply.github.com>
2024-01-25 21:40:31 -0500

    Created a quiz service and service registry with the eureka server

---
+++ quiz-service/src/main/java/com/isomer/quizservice/model/QuizDto.java
@@ -0,0 +1,10 @@
+package com.isomer.quizservice.model;
+
+import lombok.Data;
+
+@Data
+public class QuizDto {
+    String categoryName;
+    Integer numQuestions;
+    String title;
+}
---
+++ quiz-service/src/main/java/com/isomer/quizservice/model/Response.java
@@ -0,0 +1,11 @@
+package com.isomer.quizservice.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+}
